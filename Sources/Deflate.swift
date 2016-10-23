81d41bef507b538f815e26a3cda1cc8889ac48a1
Timofey Solomko <tsolomko@gmail.com>
2016-10-24 00:22:58 +0300

    Some progress was made on reading information from archive.

--- Sources/Deflate.swift
+++ Sources/Deflate.swift
@@ -53,25 +53,80 @@ public class Deflate {
     public static func decompress(data: Data) throws -> Data {
         var output = Data()
 
+        // First two bytes should be correct 'magic' bytes
         let magic = data.bytes(from: 0..<2)
         guard magic == [31, 139] else { throw DeflateError.WrongMagic }
 
+        // Third byte is a method of compression. Only type 8 (DEFLATE) compression is supported
         let method = data.byte(at: 2)
         guard method == 8 else { throw DeflateError.UnknownCompressionMethod }
 
+        // Next bytes present some service information
         let serviceInfo = ServiceInfo(magic: magic,
                                       method: method,
                                       flags: data.byte(at: 3),
                                       mtime: Data(data[4...7]).to(type: UInt64.self),
                                       extraFlags: data.byte(at: 8),
                                       osType: data.byte(at: 9))
-
         print("\(serviceInfo)")
 
-        var startPoint = 0
+        var startPoint = 10 // Index in data of 'actual data'
+
+        // Some archives may contain extra fields
         if serviceInfo.flags & Flags.fextra != 0 {
-            let xlen = Data(data[10...11]).to(type: UInt32.self)
-            print("\(xlen)")
+            let xlen = Data(data[10...11]).to(type: UInt16.self)
+            startPoint += 2 + Int(bitPattern: UInt(xlen))
+        }
+
+        // Some archives may contain source file name (this part ends with zero byte)
+        if serviceInfo.flags & Flags.fname != 0 {
+            let fnameStart = startPoint
+            while true {
+                let byte = data.byte(at: startPoint)
+                startPoint += 1
+                guard byte != 0 else { break }
+            }
+            print(String(data: data.subdata(in: fnameStart..<startPoint - 1), encoding: .utf8))
+        }
+
+        // Some archives may contain comment (this part also ends with zero)
+        if serviceInfo.flags & Flags.fcomment != 0 {
+            let fcommentStart = startPoint
+            while true {
+                let byte = data.byte(at: startPoint)
+                startPoint += 1
+                guard byte != 0 else { break }
+            }
+            print(String(data: data.subdata(in: fcommentStart..<startPoint - 1), encoding: .utf8))
+        }
+
+        // Some archives may contain 2-bytes checksum
+        if serviceInfo.flags & Flags.fhcrc != 0 {
+            let crc = Data(data[startPoint...startPoint + 1]).to(type: UInt16.self)
+            startPoint += 2
+            print("\(crc)")
+        }
+
+        while true {
+            let isLastBit = data.byte(at: startPoint)[0]
+            let blockType = data.byte(at: startPoint)[1..<3]
+
+            if blockType == [0, 0] { // Uncompressed
+
+            } else if blockType == [1, 0] || blockType == [0, 1] { // Huffman coding (either static or dynamic)
+                if blockType == [0, 1] { // Static Huffman
+                    let staticHuffmanBootstrap = [[0, 8],
+                                                  [144, 9],
+                                                  [256, 7],
+                                                  [280, 8],
+                                                  [288, -1]]
+                    let staticHuffmanLengthsBootstrap = [[0, 5],
+                                                         [32, -1]]
+                } else if blockType == [1, 0] { // Dynamic Huffman
+
+                }
+
+            }
         }
 
         return output
