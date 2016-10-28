8389ac4673fe36187c1f87e575e7201da3d42fda
Timofey Solomko <tsolomko@gmail.com>
2016-10-28 21:01:18 +0300

    Added automatic check for answer in second test.

--- Tests/Constants.swift
+++ Tests/Constants.swift
@@ -12,4 +12,5 @@ struct Constants {
     static let testBundle = Bundle(for: SWCompressionTests.self)
     static let helloWorldArchivePath = testBundle.url(forResource: "helloworld.txt", withExtension: "gz")!
     static let secondTestArchivePath = testBundle.url(forResource: "secondtest.txt", withExtension: "gz")!
+    static let secondTestAnswerPath = testBundle.url(forResource: "secondtest", withExtension: "txt")!
 }
