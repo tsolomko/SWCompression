6ee0eae306407b699f293d8cb8164f43f1e0b245
Timofey Solomko <tsolomko@gmail.com>
2016-10-29 21:17:09 +0300

    Added tests with empty file.

--- Tests/Constants.swift
+++ Tests/Constants.swift
@@ -9,8 +9,14 @@
 import Foundation
 
 struct Constants {
+
     static let testBundle = Bundle(for: SWCompressionTests.self)
+
     static let helloWorldArchivePath = testBundle.url(forResource: "helloworld.txt", withExtension: "gz")!
+
     static let secondTestArchivePath = testBundle.url(forResource: "secondtest.txt", withExtension: "gz")!
     static let secondTestAnswerPath = testBundle.url(forResource: "secondtest", withExtension: "txt")!
+
+    static let emptyFileArchivePath = testBundle.url(forResource: "empty.txt", withExtension: "gz")!
+
 }
--- Tests/SWCompressionTests.swift
+++ Tests/SWCompressionTests.swift
@@ -19,7 +19,6 @@ class SWCompressionTests: XCTestCase {
         guard decompressedData != nil else { return }
         let decompressedString = String(data: decompressedData!, encoding: .utf8)
         XCTAssertNotNil(decompressedString, "Failed to convert decompressed data to string")
-//        print("decompressed string: \(decompressedString!)")
         XCTAssertEqual(decompressedString!, "Hello, World!\n", "Decompression was incorrect")
     }
 
@@ -31,8 +30,18 @@ class SWCompressionTests: XCTestCase {
         guard decompressedData != nil else { return }
         let decompressedString = String(data: decompressedData!, encoding: .utf8)
         XCTAssertNotNil(decompressedString, "Failed to convert decompressed data to string")
-//        print("decompressed string: \(decompressedString!)")
         XCTAssertEqual(decompressedString!, try! String(contentsOf: Constants.secondTestAnswerPath, encoding: .utf8), "Decompression was incorrect")
+    }
+
+    func testEmptyFile() {
+        let testData = try? Data(contentsOf: Constants.emptyFileArchivePath)
+        XCTAssertNotNil(testData, "Failed to load test archive")
+        let decompressedData = try? Deflate.decompress(data: testData!)
+        XCTAssertNotNil(decompressedData, "Failed to decompress")
+        guard decompressedData != nil else { return }
+        let decompressedString = String(data: decompressedData!, encoding: .utf8)
+        XCTAssertNotNil(decompressedString, "Failed to convert decompressed data to string")
+        XCTAssertEqual(decompressedString!, "")
     }
 
 }
