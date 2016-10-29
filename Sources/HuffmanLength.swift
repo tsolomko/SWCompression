641c0bb13bf576c19066b943ae3eb704cdde1d89
Timofey Solomko <tsolomko@gmail.com>
2016-10-29 22:32:01 +0300

    Fixed copyright notice.

--- Sources/HuffmanLength.swift
+++ Sources/HuffmanLength.swift
@@ -3,7 +3,7 @@
 //  SWCompression
 //
 //  Created by Timofey Solomko on 26.10.16.
-//  Copyright © 2016 tsolomko. All rights reserved.
+//  Copyright © 2016 Timofey Solomko. All rights reserved.
 //
 
 import Foundation
