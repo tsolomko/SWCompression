7ff96d254308e47bd08ad84500ad621a7cd54ef9
Timofey Solomko <tsolomko@gmail.com>
2016-10-30 20:07:04 +0300

    Added support for other platforms.

--- Sources/SWCompression.h
+++ Sources/SWCompression.h
@@ -3,16 +3,14 @@
 //  SWCompression
 //
 //  Created by Timofey Solomko on 16.10.16.
-//  Copyright © 2016 tsolomko. All rights reserved.
+//  Copyright © 2016 Timofey Solomko. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
 //! Project version number for SWCompression.
-FOUNDATION_EXPORT double SWCompressionVersionNumber;
+extern double SWCompressionVersionNumber;
 
 //! Project version string for SWCompression.
-FOUNDATION_EXPORT const unsigned char SWCompressionVersionString[];
+extern const unsigned char SWCompressionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SWCompression/PublicHeader.h>
 
