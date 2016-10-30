3d69634fdf80ca44569e749f862a2b0ebdba95c0
Timofey Solomko <tsolomko@gmail.com>
2016-10-30 16:35:11 +0300

    Added documentation for protocols.

--- Sources/Protocols.swift
+++ Sources/Protocols.swift
@@ -8,14 +8,18 @@
 
 import Foundation
 
+/// Abstract archive class which supports unarchiving.
 public protocol Archive {
 
+    /// Abstract unarchive function.
     static func unarchive(archiveData: Data) throws -> Data
 
 }
 
+/// Abstract decompression algorithm class which supports decompression.
 public protocol DecompressionAlgorithm {
 
+    /// Abstract decompress function.
     static func decompress(compressedData: Data) throws -> Data
 
 }
