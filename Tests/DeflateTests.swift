91e0bcc3cd06b03e89e1b11438ea85f6b0be3085
Timofey Solomko <tsolomko@gmail.com>
2016-11-21 19:31:28 +0300

    Reorganized tests so deflate algorithm is now tested only one time.

---
+++ Tests/DeflateTests.swift
@@ -0,0 +1,160 @@
+//
+//  DeflateTests.swift
+//  SWCompression
+//
+//  Created by Timofey Solomko on 21.11.16.
+//  Copyright © 2016 tsolomko. All rights reserved.
+//
+
+import XCTest
+import SWCompression
+
+class DeflateTests: XCTestCase {
+
+    static let testType: String = "deflate"
+
+    func testDeflate1() {
+        let testName = "test1"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+    func testDeflate2() {
+        let testName = "test2"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+    func testDeflate3() {
+        let testName = "test3"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+
+    func testDeflate4() {
+        let testName = "test4"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+
+    func testDeflate5() {
+        let testName = "test5"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+
+    func testDeflate6() {
+        let testName = "test6"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+
+    func testDeflate7() {
+        let testName = "test7"
+        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: DeflateTests.testType)) else {
+            XCTFail("Failed to load test archive")
+            return
+        }
+
+        guard let decompressedData = try? Deflate.decompress(compressedData: testData) else {
+            XCTFail("Failed to decompress")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
+            XCTFail("Failed to get the answer")
+            return
+        }
+
+        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+    }
+
+}
--- Tests/ZlibTests.swift
+++ Tests/ZlibTests.swift
@@ -7,150 +7,30 @@
 //
 
 import XCTest
-import SWCompression
+@testable import SWCompression
 
 class ZlibTests: XCTestCase {
 
     static let testType: String = "zlib"
 
-    func testZlib1() {
-        let testName = "test1"
+    func testZlib() {
+        let testName = "test"
         guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
             XCTFail("Failed to load test archive")
             return
         }
 
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
+        guard let testServiceInfo = try? ZlibArchive.serviceInfo(archiveData: testData) else {
+            XCTFail("Failed to get service info")
             return
         }
 
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
+        let answerServiceInfo = ZlibArchive.ServiceInfo(compressionMethod: 8,
+                                                        windowSize: 32768,
+                                                        compressionLevel: .defaultAlgorithm,
+                                                        startPoint: 2)
 
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib2() {
-        let testName = "test2"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib3() {
-        let testName = "test3"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib4() {
-        let testName = "test4"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib5() {
-        let testName = "test5"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib6() {
-        let testName = "test6"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
-    }
-
-    func testZlib7() {
-        let testName = "test7"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: ZlibTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let decompressedData = try? ZlibArchive.unarchive(archiveData: testData) else {
-            XCTFail("Failed to decompress")
-            return
-        }
-
-        guard let answerData = try? Data(contentsOf: Constants.url(forAnswer: testName)) else {
-            XCTFail("Failed to get the answer")
-            return
-        }
-
-        XCTAssertEqual(decompressedData, answerData, "Decompression was incorrect")
+        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
     }
 
 }
