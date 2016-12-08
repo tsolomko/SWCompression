41bd64fb8a12b77f483bc818c1f69cda7601617d
Timofey Solomko <tsolomko@gmail.com>
2016-12-08 17:09:01 +0300

    filter_tests now has output filename argument.

--- filter_test.py
+++ filter_test.py
@@ -2,12 +2,13 @@ import re
 import sys
 
 filename = sys.argv[1]
+out_filename = sys.argv[2]
 inp = open(filename)
 content = inp.read()
 
 p = re.compile('( average: \d{1,3}\.\d{1,3})|(\w{3,7}\.test\d)')
 matches = p.findall(content)
-f = open('out', 'w')
+f = open(out_filename, 'w')
 for i, match in enumerate(matches):
     for s in match:
         if s != "":
