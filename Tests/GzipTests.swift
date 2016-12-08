631aab387c11f5a411b968dae80018e56a9e0f4b
Timofey Solomko <tsolomko@gmail.com>
2016-12-09 00:05:45 +0300

    Reorganized tests so there is now less duplicate code.

--- Tests/GzipTests.swift
+++ Tests/GzipTests.swift
@@ -13,8 +13,7 @@ class GzipTests: XCTestCase {
 
     static let testType: String = "gz"
 
-    func testGzip1() {
-        let testName = "test1"
+    func perform(test testName: String, answer answerServiceInfo: GzipArchive.ServiceInfo ) {
         guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
             XCTFail("Failed to load test archive")
             return
@@ -25,167 +24,92 @@ class GzipTests: XCTestCase {
             return
         }
 
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [219, 127, 19, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "test.txt",
-                                                        comment: "",
-                                                        crc: 0)
-
         XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+    }
+
+    func testGzip1() {
+        self.perform(test: "test1", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [219, 127, 19, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "test.txt",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip2() {
-        let testName = "test2"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [193, 93, 43, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "test2.answer",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test2", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [193, 93, 43, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "test2.answer",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip3() {
-        let testName = "test3"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
 
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [27, 94, 43, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "test3.answer",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test3", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [27, 94, 43, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "test3.answer",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip4() {
-        let testName = "test4"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [190, 130, 19, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "secondtest.txt",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test4", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [190, 130, 19, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "secondtest.txt",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip5() {
-        let testName = "test5"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [232, 229, 20, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "empty.txt",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test5", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [232, 229, 20, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "empty.txt",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip6() {
-        let testName = "test6"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [107, 95, 43, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "test6.answer",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test6", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [107, 95, 43, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "test6.answer",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
     func testGzip7() {
-        let testName = "test7"
-        guard let testData = try? Data(contentsOf: Constants.url(forTest: testName, withType: GzipTests.testType)) else {
-            XCTFail("Failed to load test archive")
-            return
-        }
-
-        guard let testServiceInfo = try? GzipArchive.serviceInfo(archiveData: testData) else {
-            XCTFail("Failed to get service info")
-            return
-        }
-
-        let answerServiceInfo = GzipArchive.ServiceInfo(magic: [31, 139],
-                                                        method: 8,
-                                                        flags: 8,
-                                                        mtime: [233, 74, 48, 88],
-                                                        extraFlags: 0,
-                                                        osType: 3,
-                                                        fileName: "test7.answer",
-                                                        comment: "",
-                                                        crc: 0)
-
-        XCTAssertEqual(testServiceInfo, answerServiceInfo, "Incorrect service info")
+        self.perform(test: "test7", answer: GzipArchive.ServiceInfo(magic: [31, 139],
+                                                                    method: 8,
+                                                                    flags: 8,
+                                                                    mtime: [233, 74, 48, 88],
+                                                                    extraFlags: 0,
+                                                                    osType: 3,
+                                                                    fileName: "test7.answer",
+                                                                    comment: "",
+                                                                    crc: 0))
     }
 
 }
