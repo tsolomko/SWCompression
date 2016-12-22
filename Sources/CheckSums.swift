87c0f255d537ed9b7fca3b30c281321e1ba01842
Timofey Solomko <tsolomko@gmail.com>
2016-12-22 23:50:13 +0300

    Added crc64.

--- Sources/CheckSums.swift
+++ Sources/CheckSums.swift
@@ -27,6 +27,23 @@ struct CheckSums {
         return table
     }()
 
+    static let crc64table: [UInt64] = {
+        var table: [UInt64] = Array(repeating: 0, count: 256)
+        let crc64poly: UInt64 = 0xC96C5795D7870F42
+        var r: UInt64 = 0
+        var j = 0
+        for i in 0..<256 {
+            r = UInt64(i)
+            j = 8
+            while j > 0 {
+                r = r & 1 > 0 ? (r >> 1) ^ crc64poly : r >> 1
+                j -= 1
+            }
+            table[i] = r
+        }
+        return table
+    }()
+
     static func crc32(_ array: [UInt8]) -> Int {
         var crc: UInt32 = ~0
         for i in 0..<array.count {
@@ -35,5 +52,14 @@ struct CheckSums {
         }
         return Int(~crc)
     }
-    
+
+    static func crc64(_ array: [UInt8]) -> UInt64 {
+        var crc: UInt64 = ~0
+        for i in 0..<array.count {
+            let index = (Int(crc) & 0xFF) ^ (array[i].toInt())
+            crc = CheckSums.crc64table[index] ^ (crc >> 8)
+        }
+        return UInt64(~crc)
+    }
+
 }
