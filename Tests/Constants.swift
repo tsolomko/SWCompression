75d46c53f393ebf2654004f065c8c1ebd9985be5
Timofey Solomko <tsolomko@gmail.com>
2016-12-24 22:34:08 +0300

    Added description of test files.

--- Tests/Constants.swift
+++ Tests/Constants.swift
@@ -10,6 +10,18 @@ import Foundation
 
 struct Constants {
 
+    /* Contents of test files:
+     - test1: text file with "Hello, World!\n".
+     - test2: text file with copyright free song lyrics from http://www.freesonglyrics.co.uk/lyrics13.html
+     - test3: text file with random string from https://www.random.org/strings/
+     - test4: text file with string "I'm a tester" repeated several times.
+     - test5: empty file.
+     - test6: file with size of 5MB containing nulls from /dev/null.
+     - test7: file with size of 5MB containing random bytes from /dev/urandom.
+     - test8: text file from lzma_specification.
+     - random_file: file with size of 10KB containing random bytes from /dev/urandom.
+    */
+
     static func url(forTest name: String, withType ext: String) -> URL {
         return testBundle.url(forResource: name, withExtension: ext)!
     }
