e8c2bf992da56299ac55e8ceaeff3b0e632a5d29
Timofey Solomko <tsolomko@gmail.com>
2016-12-24 20:33:03 +0300

    Added check of header’s crc.

--- Sources/GzipArchive.swift
+++ Sources/GzipArchive.swift
@@ -15,8 +15,10 @@ import Foundation
  - `WrongMagic`: first two bytes of archive were not 31 and 139.
  - `WrongCompressionMethod`: unsupported compression method (not 8 aka Deflate).
  - `WrongFlags`: unsupported flags (reserved flags weren't 0).
- - `WrongCRC`: computed Cyclic Redundancy Check didn't match the value stored in the archive.
- - `WrongISize`: size of uncompressed data modulo 2^32 didn't match the value stored in the archive.
+ - `WrongHeaderCRC`: computed Cyclic Redundancy Check of archive's header
+        didn't match the archive's value.
+ - `WrongCRC`: computed Cyclic Redundancy Check of uncompressed data didn't match the archive's value.
+ - `WrongISize`: size of uncompressed data modulo 2^32 didn't match the archive's value.
  */
 public enum GzipError: Error {
     /// First two bytes of archive were not 31 and 139.
@@ -25,6 +27,8 @@ public enum GzipError: Error {
     case WrongCompressionMethod
     /// Reserved flags bits were not equal to 0.
     case WrongFlags
+    /// Computed CRC of header didn't match the value stored in the archive.
+    case WrongHeaderCRC
     /// Computed CRC of uncompressed data didn't match the value stored in the archive.
     case WrongCRC
     /// Computed isize didn't match the value stored in the archive.
@@ -70,18 +74,20 @@ public class GzipArchive: Archive {
 
     static func serviceInfo(_ pointerData: DataWithPointer) throws -> ServiceInfo {
         // First two bytes should be correct 'magic' bytes
-        let magic = pointerData.intFromBits(count: 16)
+        let magic = pointerData.intFromAlignedBytes(count: 2)
         guard magic == 0x8b1f else { throw GzipError.WrongMagic }
+        var headerBytes: [UInt8] = [0x1f, 0x8b]
 
         // Third byte is a method of compression. Only type 8 (DEFLATE) compression is supported
         let method = pointerData.alignedByte()
         guard method == 8 else { throw GzipError.WrongCompressionMethod }
+        headerBytes.append(method)
 
         // Next bytes present some service information
         var serviceInfo = ServiceInfo(magic: magic,
                                       method: method,
                                       flags: pointerData.alignedByte(),
-                                      mtime: pointerData.intFromBits(count: 32),
+                                      mtime: pointerData.intFromAlignedBytes(count: 4),
                                       extraFlags: pointerData.alignedByte(),
                                       osType: pointerData.alignedByte(),
                                       fileName: "", comment: "", crc: 0)
@@ -89,12 +95,23 @@ public class GzipArchive: Archive {
         guard (serviceInfo.flags & 0x20 == 0) &&
             (serviceInfo.flags & 0x40 == 0) &&
             (serviceInfo.flags & 0x80 == 0) else { throw GzipError.WrongFlags }
+        headerBytes.append(serviceInfo.flags)
+
+        for i in 0..<4 {
+            headerBytes.append(((serviceInfo.mtime & (0xFF << (i * 8))) >> (i * 8)).toUInt8())
+        }
+        headerBytes.append(serviceInfo.extraFlags)
+        headerBytes.append(serviceInfo.osType)
 
         // Some archives may contain extra fields
         if serviceInfo.flags & Flags.fextra != 0 {
-            let xlen = pointerData.intFromBits(count: 16)
-            pointerData.index += xlen
-            // TODO: Add extra fields' processing
+            let xlen = pointerData.intFromAlignedBytes(count: 2)
+            for i in 0..<2 {
+                headerBytes.append(((xlen & (0xFF << (i * 8))) >> (i * 8)).toUInt8())
+            }
+            for _ in 0..<xlen {
+                headerBytes.append(pointerData.alignedByte())
+            }
         }
 
         // Some archives may contain source file name (this part ends with zero byte)
@@ -104,6 +121,7 @@ public class GzipArchive: Archive {
                 let byte = pointerData.alignedByte()
                 guard byte != 0 else { break }
                 fnameBytes.append(byte)
+                headerBytes.append(byte)
             }
             serviceInfo.fileName = String(data: Data(fnameBytes), encoding: .utf8) ?? ""
         }
@@ -115,13 +133,17 @@ public class GzipArchive: Archive {
                 let byte = pointerData.alignedByte()
                 guard byte != 0 else { break }
                 fcommentBytes.append(byte)
+                headerBytes.append(byte)
             }
             serviceInfo.comment = String(data: Data(fcommentBytes), encoding: .utf8) ?? ""
         }
 
         // Some archives may contain 2-bytes checksum
         if serviceInfo.flags & Flags.fhcrc != 0 {
-            serviceInfo.crc = pointerData.intFromBits(count: 16)
+            // Note: it is not actual CRC-16, it is just two least significant bytes of CRC-32.
+            let crc16 = pointerData.intFromAlignedBytes(count: 2)
+            let ourCRC32 = CheckSums.crc32(headerBytes)
+            guard ourCRC32 & 0xFFFF == crc16 else { throw GzipError.WrongHeaderCRC }
         }
 
         return serviceInfo
