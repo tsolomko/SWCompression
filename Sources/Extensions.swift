d877adf9622b1b656880c71dc239f0719422ba52
Timofey Solomko <tsolomko@gmail.com>
2017-01-05 23:09:53 +0300

    Updated copyright year.

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -3,7 +3,7 @@
 //  SWCompression
 //
 //  Created by Timofey Solomko on 19.09.16.
-//  Copyright © 2016 Timofey Solomko. All rights reserved.
+//  Copyright © 2017 Timofey Solomko. All rights reserved.
 //
 
 import Foundation
