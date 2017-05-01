5c8fd61a0a4b65846600be6c344c55b1e35d48df
Timofey Solomko <tsolomko@gmail.com>
2017-05-01 21:28:02 +0300

    GzipTests now test performance on gzip unarchiving.

--- Tests/GzipTests.swift
+++ Tests/GzipTests.swift
@@ -60,7 +60,7 @@ class GzipTests: XCTestCase {
         #if PERF_TESTS
             print("Performing performance tests for gz.\(testName)")
             self.measure {
-                _ = try? Deflate.decompress(compressedData: testData)
+                _ = try? GzipArchive.unarchive(archiveData: archiveData)
             }
         #endif
     }
