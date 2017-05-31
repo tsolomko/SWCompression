a5b3f5c6c1bcf29008435be60db3450b95c847d4
Timofey Solomko <tsolomko@gmail.com>
2017-05-31 19:12:38 +0300

    Edited LZMA related documentation.

--- Sources/LZMA.swift
+++ Sources/LZMA.swift
@@ -9,26 +9,15 @@
 import Foundation
 
 /**
- Error happened during LZMA decompression.
- It may indicate that either the data is damaged or it might not be compressed with LZMA at all.
-
- - `wrongProperties`: unsupported LZMA properties (greater than 225).
- - `rangeDecoderInitError`: unable to initialize RangedDecoder.
- - `exceededUncompressedSize`: the number of uncompressed bytes reached amount specified by archive
-    while decoding wasn't finished.
- - `windowIsEmpty`: unable to repeat bytes because there is nothing to repeat.
- - `rangeDecoderFinishError`: range decoder was in a bad state when finish marker was reached.
- - `repeatWillExceed`: unable to repeat bytes because the number of bytes to repeat is greater
-    than the amount bytes that is left to decode.
- - `notEnoughToRepeat`: unable to repeat bytes because the amount of already decoded bytes is smaller
-    than the repeat length.
+ Represents an error, which happened during LZMA decompression.
+ It may indicate that either data is damaged or it might not be compressed with LZMA at all.
  */
 public enum LZMAError: Error {
-    /// Properties byte was greater than 225.
+    /// Properties' byte is greater than 225.
     case wrongProperties
     /// Unable to initialize RanderDecorer.
     case rangeDecoderInitError
-    /// The number of uncompressed bytes hit limit in the middle of decoding.
+    /// Size of uncompressed data hit specified limit in the middle of decoding.
     case exceededUncompressedSize
     /// Unable to perfrom repeat-distance decoding because there is nothing to repeat.
     case windowIsEmpty
@@ -38,22 +27,22 @@ public enum LZMAError: Error {
     case repeatWillExceed
     /// The amount of already decoded bytes is smaller than repeat length.
     case notEnoughToRepeat
-
+    /// LZMADecoder wasn't properly initialized before decoding data.
     case decoderIsNotInitialised
 }
 
-/// Provides function to decompress data, which were compressed with LZMA
+/// Provides decompression function for LZMA algorithm.
 public class LZMA: DecompressionAlgorithm {
 
     /**
-     Decompresses `compressedData` with LZMA algortihm.
+     Decompresses `data` using LZMA algortihm.
 
-     If data passed is not actually compressed with LZMA, `LZMAError` will be thrown.
+     If `data` is not actually compressed with LZMA, `LZMAError` will be thrown.
 
-     - Parameter compressedData: Data compressed with LZMA.
+     - Parameter data: Data compressed with LZMA.
 
-     - Throws: `LZMAError` if unexpected byte (bit) sequence was encountered in `compressedData`.
-     It may indicate that either the data is damaged or it might not be compressed with LZMA at all.
+     - Throws: `LZMAError` if unexpected byte (bit) sequence was encountered in `data`.
+     It may indicate that either data is damaged or it might not be compressed with LZMA at all.
 
      - Returns: Decompressed data.
      */
