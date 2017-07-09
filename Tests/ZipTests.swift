a9ab32758397d234e05270d6cd30b74464e35e4c
Timofey Solomko <tsolomko@gmail.com>
2017-07-09 13:50:44 +0300

    Add test of zip file with BZip2 compression

--- Tests/ZipTests.swift
+++ Tests/ZipTests.swift
@@ -142,4 +142,41 @@ class ZipTests: XCTestCase {
         XCTAssertEqual(try? entries[0].data(), answerData)
     }
 
+    func testZipBZip2() {
+        guard let testURL = Constants.url(forTest: "test_zip_bzip2", withType: ZipTests.testType) else {
+            XCTFail("Unable to get test's URL.")
+            return
+        }
+
+        guard let testData = try? Data(contentsOf: testURL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load test archive.")
+            return
+        }
+
+        guard let entries = try? ZipContainer.open(container: testData) else {
+            XCTFail("Unable to open ZIP archive.")
+            return
+        }
+
+        guard entries.count == 1 else {
+            XCTFail("Incorrect number of entries.")
+            return
+        }
+
+        XCTAssertEqual(entries[0].name, "test4.answer")
+        XCTAssertEqual(entries[0].isDirectory, false)
+
+        guard let answerURL = Constants.url(forAnswer: "test4") else {
+            XCTFail("Unable to get answer's URL.")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: answerURL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load answer.")
+            return
+        }
+
+        XCTAssertEqual(try? entries[0].data(), answerData)
+    }
+
 }
