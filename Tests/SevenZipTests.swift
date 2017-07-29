b01e3c99d3cf775588ff10978b0ca6631702d520
Timofey Solomko <tsolomko@gmail.com>
2017-07-29 23:22:18 +0300

    Rework 7z tests to also use open function, provide long versions of them and use 7z container with SWCompression source code

--- Tests/SevenZipTests.swift
+++ Tests/SevenZipTests.swift
@@ -21,11 +21,26 @@ class SevenZipTests: XCTestCase {
             return
         }
 
-        guard let infos = try? SevenZipContainer.info(container: testData) else {
+        guard let entries = try? SevenZipContainer.open(container: testData) else {
             XCTFail("Unable to open 7z archive.")
             return
         }
-        print(infos)
+
+        guard let answerURL = Constants.url(forAnswer: "test1") else {
+            XCTFail("Unable to get answer's URL.")
+            return
+        }
+
+        guard let answerData = try? Data(contentsOf: answerURL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load answer.")
+            return
+        }
+
+        XCTAssertEqual(entries.count, 1)
+        XCTAssertEqual(entries[0].name, "test1.answer")
+        XCTAssertEqual(entries[0].isDirectory, false)
+        XCTAssertEqual(entries[0].size, answerData.count)
+        XCTAssertEqual(try? entries[0].data(), answerData)
     }
 
     func test2() {
@@ -39,11 +54,43 @@ class SevenZipTests: XCTestCase {
             return
         }
 
-        guard let infos = try? SevenZipContainer.info(container: testData) else {
+        guard let entries = try? SevenZipContainer.open(container: testData) else {
             XCTFail("Unable to open 7z archive.")
             return
         }
-        print(infos)
+
+        XCTAssertEqual(entries.count, 2)
+
+        guard let answer1URL = Constants.url(forAnswer: "test1") else {
+            XCTFail("Unable to get answer's URL.")
+            return
+        }
+
+        guard let answer1Data = try? Data(contentsOf: answer1URL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load answer.")
+            return
+        }
+
+        XCTAssertEqual(entries[0].name, "test1.answer")
+        XCTAssertEqual(entries[0].isDirectory, false)
+        XCTAssertEqual(entries[0].size, answer1Data.count)
+        XCTAssertEqual(try? entries[0].data(), answer1Data)
+
+
+        guard let answer4URL = Constants.url(forAnswer: "test4") else {
+            XCTFail("Unable to get answer's URL.")
+            return
+        }
+
+        guard let answer4Data = try? Data(contentsOf: answer4URL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load answer.")
+            return
+        }
+
+        XCTAssertEqual(entries[0].name, "test4.answer")
+        XCTAssertEqual(entries[0].isDirectory, false)
+        XCTAssertEqual(entries[0].size, answer4Data.count)
+        XCTAssertEqual(try? entries[0].data(), answer4Data)
     }
 
     func test3() {
@@ -57,11 +104,29 @@ class SevenZipTests: XCTestCase {
             return
         }
 
-        guard let infos = try? SevenZipContainer.info(container: testData) else {
-            XCTFail("Unable to open 7z archive.")
+        #if LONG_TESTS
+            XCTAssertNotNil(try? SevenZipContainer.open(container: testData))
+        #else
+            XCTAssertNotNil(try? SevenZipContainer.info(container: testData))
+        #endif
+    }
+
+    func testBigContainer() {
+        guard let testURL = Constants.url(forTest: "SWCompressionSourceCode", withType: SevenZipTests.testType) else {
+            XCTFail("Unable to get test's URL.")
             return
         }
-        print(infos)
+
+        guard let testData = try? Data(contentsOf: testURL, options: .mappedIfSafe) else {
+            XCTFail("Unable to load test archive.")
+            return
+        }
+
+        #if LONG_TESTS
+            XCTAssertNotNil(try? SevenZipContainer.open(container: testData))
+        #else
+            XCTAssertNotNil(try? SevenZipContainer.info(container: testData))
+        #endif
     }
 
 }
