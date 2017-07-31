0bc1d470e948a24d9b9f0d4db34dd8b14d400251
Timofey Solomko <tsolomko@gmail.com>
2017-07-31 18:55:56 +0300

    Add some types for permissions/attributes

--- Sources/7zEntryInfo.swift
+++ Sources/7zEntryInfo.swift
@@ -7,6 +7,59 @@ import Foundation
 
 public struct SevenZipEntryInfo {
 
+    public struct Permissions: OptionSet {
+        
+        public let rawValue: UInt32
+
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+
+        public static let setuid = Permissions(rawValue: 0o4000)
+        public static let setgid = Permissions(rawValue: 0o2000)
+        public static let sticky = Permissions(rawValue: 0o1000)
+
+        public static let readOwner = Permissions(rawValue: 0o0400)
+        public static let writeOwner = Permissions(rawValue: 0o0200)
+        public static let executeOwner = Permissions(rawValue: 0o0100)
+
+        public static let readGroup = Permissions(rawValue: 0o0040)
+        public static let writeGroup = Permissions(rawValue: 0o0020)
+        public static let executeGroup = Permissions(rawValue: 0o0010)
+
+        public static let readOther = Permissions(rawValue: 0o0004)
+        public static let writeOther = Permissions(rawValue: 0o0002)
+        public static let executeOther = Permissions(rawValue: 0o0001)
+
+    }
+
+    public struct DosAttributes: OptionSet {
+
+        public let rawValue: UInt32
+
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+
+        public static let archive = DosAttributes(rawValue: 0b00100000)
+        public static let directory = DosAttributes(rawValue: 0b00010000)
+        public static let volume = DosAttributes(rawValue: 0b00001000)
+        public static let system = DosAttributes(rawValue: 0b00000100)
+        public static let hidden = DosAttributes(rawValue: 0b00000010)
+        public static let readOnly = DosAttributes(rawValue: 0b00000001)
+
+    }
+
+    public enum UnixType: UInt32 {
+        case fifo = 0o010000
+        case characterSpecial = 0o020000
+        case directory = 0o040000
+        case blockSpecial = 0o060000
+        case regular = 0o100000
+        case symbolicLink = 0o120000
+        case socket = 0o140000
+    }
+
     public let name: String?
     public let size: Int?
     public let isDirectory: Bool
