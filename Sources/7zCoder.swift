a65666cd19264fb450a7544fee7f027e4211b93c
Timofey Solomko <tsolomko@gmail.com>
2017-08-14 17:29:58 +0300

    Remove unused coder IDs and extra comments

--- Sources/7zCoder.swift
+++ Sources/7zCoder.swift
@@ -9,19 +9,15 @@ class SevenZipCoder {
 
     /// Possible coder IDs.
     struct ID {
-    // TODO: Left comments column indicates whether coder has been implemented or not (I/NI) and tested (T/NT).
-    /*  I/T  */static let copy: [UInt8] = [0x00]
+        static let copy: [UInt8] = [0x00]
         /// Shouldn't be used, added for compatibility, `ID.copy` should be used instead.
-    /*  I/T  */static let zipCopy: [UInt8] = [0x04, 0x01, 0x00]
-    /*  I/T  */static let deflate: [UInt8] = [0x04, 0x01, 0x08]
+        static let zipCopy: [UInt8] = [0x04, 0x01, 0x00]
+        static let deflate: [UInt8] = [0x04, 0x01, 0x08]
         /// Shouldn't be used, added for compatibility, `ID.bzip2` should be used instead.
-    /*  I/T  */static let zipBzip2: [UInt8] = [0x04, 0x01, 0x0C]
-    /* NI/NT */static let zipLzma: [UInt8] = [0x04, 0x01, 0x0E]
-        /// For some reason, this method is specified in ZIP section of Methods.txt.
-    /* NI/NT */static let xz: [UInt8] = [0x04, 0x01, 0x5F]
-    /*  I/T  */static let bzip2: [UInt8] = [0x04, 0x02, 0x02]
-    /*  I/T  */static let lzma2: [UInt8] = [0x21]
-    /*  I/T  */static let lzma: [UInt8] = [0x03, 0x01, 0x01]
+        static let zipBzip2: [UInt8] = [0x04, 0x01, 0x0C]
+        static let bzip2: [UInt8] = [0x04, 0x02, 0x02]
+        static let lzma2: [UInt8] = [0x21]
+        static let lzma: [UInt8] = [0x03, 0x01, 0x01]
     }
 
     let idSize: Int
