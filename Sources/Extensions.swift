a1daa622a577f420282f4b24203b29a55e8c2a57
Timofey Solomko <tsolomko@gmail.com>
2017-08-16 22:16:22 +0300

    Swiftlinted

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -74,12 +74,12 @@ extension Date {
         if let time = ntfsTime,
             let ntfsStartDate = DateComponents(calendar: Calendar(identifier: .iso8601),
                                                timeZone: TimeZone(abbreviation: "UTC"),
-                                               year: 1601, month: 1,day: 1,
+                                               year: 1601, month: 1, day: 1,
                                                hour: 0, minute: 0, second: 0).date {
             self.init(timeInterval: TimeInterval(time) / 10_000_000, since: ntfsStartDate)
         } else {
             return nil
         }
     }
-    
+
 }
