5d19120b63f87944617afd50bc69841156fad032
Timofey Solomko <tsolomko@gmail.com>
2017-09-03 14:00:26 +0300

    Add inits from new HuffmanLengths to HuffmanTrees

--- Sources/DecodingHuffmanTree.swift
+++ Sources/DecodingHuffmanTree.swift
@@ -12,6 +12,62 @@ class DecodingHuffmanTree {
     private var tree: [Int]
     private let leafCount: Int
 
+    /// `lengths` don't have to be properly sorted.
+    init(lengths: [HuffmanLength], _ bitReader: BitReader) {
+        self.bitReader = bitReader
+
+        // Sort `lengths` array to calculate canonical Huffman code.
+        let sortedLengths = lengths.sorted { (left: HuffmanLength, right: HuffmanLength) -> Bool in
+            if left.codeLength == right.codeLength {
+                return left.symbol < right.symbol
+            } else {
+                return left.codeLength < right.codeLength
+            }
+        }
+
+        func reverse(bits: Int, in symbol: Int) -> Int {
+            // Auxiliarly function, which generates reversed order of bits in a number.
+            var a = 1 << 0
+            var b = 1 << (bits - 1)
+            var z = 0
+            for i in stride(from: bits - 1, to: -1, by: -2) {
+                z |= (symbol >> i) & a
+                z |= (symbol << i) & b
+                a <<= 1
+                b >>= 1
+            }
+            return z
+        }
+
+        // Calculate maximum amount of leaves possible in a tree.
+        self.leafCount = 1 << (sortedLengths.last!.codeLength + 1)
+        self.tree = Array(repeating: -1, count: leafCount)
+
+        // Calculates symbols for each length in 'sortedLengths' array and put them in the tree.
+        var loopBits = -1
+        var symbol = -1
+        for length in sortedLengths {
+            symbol += 1
+            // We sometimes need to make symbol to have length.bits bit length.
+            let bits = length.codeLength
+            if bits != loopBits {
+                symbol <<= (bits - loopBits)
+                loopBits = bits
+            }
+            // Then we need to reverse bit order of the symbol.
+            var treeCode = reverse(bits: loopBits, in: symbol)
+
+            // Finally, we put it at its place in the tree.
+            var index = 0
+            for _ in 0..<bits {
+                let bit = treeCode & 1
+                index = bit == 0 ? 2 * index + 1 : 2 * index + 2
+                treeCode >>= 1
+            }
+            self.tree[index] = length.symbol
+        }
+    }
+
     init(bootstrap: [(symbol: Int, codeLength: Int)], _ bitReader: BitReader) {
         self.bitReader = bitReader
 
--- Sources/EncodingHuffmanTree.swift
+++ Sources/EncodingHuffmanTree.swift
@@ -11,6 +11,52 @@ class EncodingHuffmanTree {
 
     private var codingIndices: [[Int]]
 
+    /// `lengths` don't have to be properly sorted.
+    init(lengths: [HuffmanLength], _ bitWriter: BitWriter) {
+        self.bitWriter = bitWriter
+
+        // Sort `lengths` array to calculate canonical Huffman code.
+        let sortedLengths = lengths.sorted { (left: HuffmanLength, right: HuffmanLength) -> Bool in
+            if left.codeLength == right.codeLength {
+                return left.symbol < right.symbol
+            } else {
+                return left.codeLength < right.codeLength
+            }
+        }
+
+        func reverse(bits: Int, in symbol: Int) -> Int {
+            // Auxiliarly function, which generates reversed order of bits in a number.
+            var a = 1 << 0
+            var b = 1 << (bits - 1)
+            var z = 0
+            for i in stride(from: bits - 1, to: -1, by: -2) {
+                z |= (symbol >> i) & a
+                z |= (symbol << i) & b
+                a <<= 1
+                b >>= 1
+            }
+            return z
+        }
+
+        self.codingIndices = Array(repeating: [-1, -1], count: sortedLengths.count)
+
+        // Calculates symbols for each length in 'sortedLengths' array and put them in the tree.
+        var loopBits = -1
+        var symbol = -1
+        for length in sortedLengths {
+            symbol += 1
+            // We sometimes need to make symbol to have length.bits bit length.
+            let bits = length.codeLength
+            if bits != loopBits {
+                symbol <<= (bits - loopBits)
+                loopBits = bits
+            }
+            // Then we need to reverse bit order of the symbol.
+            let treeCode = reverse(bits: loopBits, in: symbol)
+            self.codingIndices[length.symbol] = [treeCode, bits]
+        }
+    }
+
     init(bootstrap: [(symbol: Int, codeLength: Int)], _ bitWriter: BitWriter) {
         self.bitWriter = bitWriter
 
