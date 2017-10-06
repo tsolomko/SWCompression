bc523660b7be7190d7617a26212d9e6886623119
Timofey Solomko <tsolomko@gmail.com>
2017-10-06 23:36:36 +0300

    Update SPM manifest to version 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "SWCompression",
-    swiftLanguageVersions: [3],
-    exclude: [
-      "Sources/Service/",
-      "Tests/"
-    ]
+    products: [
+        .library(
+            name: "SWCompression",
+            targets: ["SWCompression"]),
+    ],
+    targets: [
+        .target(
+            name: "SWCompression",
+            path: "Sources",
+            sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZMA", "TAR", "XZ", "ZIP", "Zlib"])
+    ],
+    swiftLanguageVersions: [4]
 )
