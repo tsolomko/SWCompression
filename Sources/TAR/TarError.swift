9c44244b2423faaa98aa9ca1d8c14069a5363764
Timofey Solomko <tsolomko@gmail.com>
2017-10-08 20:19:40 +0300

    Remove TarError.wrongUstarVersion

--- Sources/TAR/TarError.swift
+++ Sources/TAR/TarError.swift
@@ -5,8 +5,6 @@
 
 import Foundation
 
-// TODO: Remove wrongUstarVersion error in 4.0.
-
 /**
  Represents an error, which happened during processing TAR container.
  It may indicate that either container is damaged or it might not be TAR container at all.
@@ -18,8 +16,6 @@ public enum TarError: Error {
     case fieldIsNotNumber
     /// Computed checksum of a header doesn't match the value stored in container.
     case wrongHeaderChecksum
-    /// Unsupported version of USTAR format.
-    case wrongUstarVersion
     /// Entry from PAX extended header is in incorrect format.
     case wrongPaxHeaderEntry
     /// Failed to process a field as an ASCII string.
