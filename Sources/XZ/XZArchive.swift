4e6f5f368a2ceba8cfab2f8182d0e7a1b829cdd5
Timofey Solomko <tsolomko@gmail.com>
2017-10-08 22:10:14 +0300

    Extract XZ related DataWithPointer extension into separate file

--- Sources/XZ/XZArchive.swift
+++ Sources/XZ/XZArchive.swift
@@ -304,25 +304,3 @@ public class XZArchive: Archive {
     }
 
 }
-
-extension DataWithPointer {
-
-    func multiByteDecode() throws -> Int {
-        var i = 1
-        var result = self.byte().toInt()
-        if result <= 127 {
-            return result
-        }
-        result &= 0x7F
-        while self.previousByte & 0x80 != 0 {
-            let byte = self.byte()
-            if i >= 9 || byte == 0x00 {
-                throw XZError.multiByteIntegerError
-            }
-            result += (byte.toInt() & 0x7F) << (7 * i)
-            i += 1
-        }
-        return result
-    }
-
-}
