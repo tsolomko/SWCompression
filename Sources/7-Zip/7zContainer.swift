66cf5ee116da50dd405b8117a3492757e95aba0c
Timofey Solomko <tsolomko@gmail.com>
2017-10-08 22:07:52 +0300

    Extract 7z related BitReader extension into separate file

--- Sources/7-Zip/7zContainer.swift
+++ Sources/7-Zip/7zContainer.swift
@@ -281,36 +281,3 @@ public class SevenZipContainer {
     }
 
 }
-
-extension BitReader {
-
-    /// Abbreviation for "sevenZipMultiByteDecode".
-    func szMbd() -> Int {
-        self.align()
-        let firstByte = self.byte().toInt()
-        var mask = 0x80
-        var value = 0
-        for i in 0..<8 {
-            if firstByte & mask == 0 {
-                value |= ((firstByte & (mask &- 1)) << (8 * i))
-                break
-            }
-            value |= self.byte().toInt() << (8 * i)
-            mask >>= 1
-        }
-        return value
-    }
-
-    func defBits(count: Int) -> [UInt8] {
-        self.align()
-        let allDefined = self.byte()
-        let definedBits: [UInt8]
-        if allDefined == 0 {
-            definedBits = self.bits(count: count)
-        } else {
-            definedBits = Array(repeating: 1, count: count)
-        }
-        return definedBits
-    }
-
-}
