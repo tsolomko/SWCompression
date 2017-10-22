f9f4d71ec8f8b94bfa18c2b4fd57dd1e21132dc5
Timofey Solomko <tsolomko@gmail.com>
2017-10-22 13:28:30 +0300

    Remove swcomp revision and update swcomp's description

--- Sources/swcomp/main.swift
+++ Sources/swcomp/main.swift
@@ -7,14 +7,12 @@ import Foundation
 import SWCompression
 import SwiftCLI
 
-/* TODO: Switch to usage of Bundle.allBundles() function of Foundation framework when it becomes implemented.*/
-// Version constants:
-let SWCompressionVersion = "3.4.0"
-let swcompRevision = "88"
-
 CLI.setup(name: "swcomp",
-          version: "\(swcompRevision), SWCompression version: \(SWCompressionVersion)",
-          description: "swcomp - small command-line client for SWCompression framework.")
+          version: "3.4.0",
+          description: """
+                       swcomp - small command-line client for SWCompression framework.
+                       Serves as an example of SWCompression usage.
+                       """)
 SwiftCLI.GlobalOptions.source(GlobalOptions.self)
 CLI.register(commands: [XZCommand(),
                         LZMACommand(),
