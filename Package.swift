79b4e81a49e0ed27f7a09076a5b9fb1c2fed41a2
Timofey Solomko <tsolomko@gmail.com>
2017-11-11 12:06:34 +0300

    Do not build swcomp by default with SPM

--- Package.swift
+++ Package.swift
@@ -8,19 +8,21 @@ let package = Package(
             name: "SWCompression",
             targets: ["SWCompression"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "4.0.0")
-    ],
+    // SWCOMP: Uncomment the lines below to build swcomp example program.
+    // dependencies: [
+    //     .package(url: "https://github.com/jakeheis/SwiftCLI", from: "4.0.0")
+    // ],
     targets: [
+        // SWCOMP: Uncomment the lines below to build swcomp example program.
+        // .target(
+        //     name: "swcomp",
+        //     dependencies: ["SWCompression", "SwiftCLI"],
+        //     path: "Sources",
+        //     sources: ["swcomp"]),
         .target(
             name: "SWCompression",
             path: "Sources",
-            sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"]),
-        .target(
-            name: "swcomp",
-            dependencies: ["SWCompression", "SwiftCLI"],
-            path: "Sources",
-            sources: ["swcomp"])
+            sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"])
     ],
     swiftLanguageVersions: [4]
 )
