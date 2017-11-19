733b7a26b389fbd8e38f2a5f06eacfb15f6e1d46
Timofey Solomko <tsolomko@gmail.com>
2017-11-19 20:05:21 +0300

    Remove empty_file special case in getting test data

--- Tests/Constants.swift
+++ Tests/Constants.swift
@@ -12,7 +12,6 @@ struct Constants {
      - test2: text file with copyright free song lyrics from http://www.freesonglyrics.co.uk/lyrics13.html
      - test3: text file with random string from https://www.random.org/strings/
      - test4: text file with string "I'm a tester" repeated several times.
-     - test5: empty file. // TODO: Rename to empty_file
      - test6: file with size of 5MB containing nulls from /dev/null.
      - test7: file with size of 5MB containing random bytes from /dev/urandom.
      - test8: text file from lzma_specification.
@@ -32,10 +31,6 @@ struct Constants {
     }
 
     static func data(forAnswer name: String) -> Data? {
-        if name == "empty_file" {
-            return Data()
-        }
-
         if let url = Constants.url(forAnswer: name) {
             return try? Data(contentsOf: url, options: .mappedIfSafe)
         } else {
