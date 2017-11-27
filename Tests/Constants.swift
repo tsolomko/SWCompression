43d9b2c1f83958492e528cebd1ef05f069939ac9
Timofey Solomko <tsolomko@gmail.com>
2017-11-27 21:36:36 +0300

    Mention test5 in Constants again

--- Tests/Constants.swift
+++ Tests/Constants.swift
@@ -12,6 +12,7 @@ class Constants {
      - test2: text file with copyright free song lyrics from http://www.freesonglyrics.co.uk/lyrics13.html
      - test3: text file with random string from https://www.random.org/strings/
      - test4: text file with string "I'm a tester" repeated several times.
+     - test5: empty file.
      - test6: file with size of 1MB containing nulls from /dev/zero.
      - test7: file with size of 1MB containing random bytes from /dev/urandom.
      - test8: text file from lzma_specification.
