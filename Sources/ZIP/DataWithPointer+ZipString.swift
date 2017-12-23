13d411e47c99cbc8757f6ce425e4815ab3874479
Timofey Solomko <tsolomko@gmail.com>
2017-12-23 21:21:14 +0300

    Optimise zip's needsUtf8 function

--- Sources/ZIP/DataWithPointer+ZipString.swift
+++ Sources/ZIP/DataWithPointer+ZipString.swift
@@ -14,7 +14,7 @@ extension DataWithPointer {
     func getZipStringField(_ length: Int, _ useUtf8: Bool) -> String? {
         guard length > 0
             else { return "" }
-        let stringData = Data(self.data[self.index..<self.index + length]) // TODO: Remove explicit copying?
+        let stringData = self.data[self.index..<self.index + length]
         self.index += length
         if useUtf8 {
             return String(data: stringData, encoding: .utf8)
