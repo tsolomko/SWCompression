0b922ba64cc576b1054f58ced544a9a0d84e9c4c
Timofey Solomko <tsolomko@gmail.com>
2018-01-03 19:23:15 +0300

    Fixes for deployment stage in CI
    Use osx and run before-deploy only once; don't carthage archive bcsymbolmap files from BitByteData; prepare-workspace before before_deploy script.

--- Utils/ci-before-deploy.sh
+++ Utils/ci-before-deploy.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+(set +x; echo "=> Removing dependencies\' bcsymbolmap files.")
+rm -f Carthage/Build/Mac/*.bcsymbolmap
+rm -f Carthage/Build/watchOS/*.bcsymbolmap
+rm -f Carthage/Build/tvOS/*.bcsymbolmap
+rm -f Carthage/Build/iOS/*.bcsymbolmap
+(set +x; echo "=> Preparing deployment files.")
 carthage build --no-skip-current
 carthage archive SWCompression
 swift build
