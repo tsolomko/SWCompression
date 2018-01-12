fe3f1df588881c0cbedf705a942762e5e878d64b
Timofey Solomko <tsolomko@gmail.com>
2018-01-12 21:41:19 +0300

    [TAR] Remove Int.octalToDecimal() extension function, since it isn't used anymore

--- Sources/TAR/Int+Tar.swift
+++ Sources/TAR/Int+Tar.swift
@@ -7,18 +7,6 @@ import Foundation
 
 extension Int {
 
-    func octalToDecimal() -> Int {
-        var octal = self
-        var decimal = 0, i = 0
-        while octal != 0 {
-            let remainder = octal % 10
-            octal /= 10
-            decimal += remainder * Int(pow(8, Double(i)))
-            i += 1
-        }
-        return decimal
-    }
-
     func roundTo512() -> Int {
         let fractionNum = Double(self) / 512
         let roundedNum = Int(ceil(fractionNum))
