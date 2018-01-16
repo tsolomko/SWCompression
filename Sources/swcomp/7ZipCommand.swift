6781ff5e966506943a033a8b9516cd406eb75128
Timofey Solomko <tsolomko@gmail.com>
2018-01-16 22:51:28 +0300

    [swcomp] Implement ZipCommand and SevenZipCommand using ContainerCommand

--- Sources/swcomp/7ZipCommand.swift
+++ Sources/swcomp/7ZipCommand.swift
@@ -7,39 +7,18 @@ import Foundation
 import SWCompression
 import SwiftCLI
 
-class SevenZipCommand: Command {
+class SevenZipCommand: ContainerCommand {
 
     let name = "7z"
     let shortDescription = "Extracts 7-Zip container"
 
     let info = Flag("-i", "--info", description: "Print list of entries in container and their attributes")
     let extract = Key<String>("-e", "--extract", description: "Extract container into specified directory")
-
     let verbose = Flag("--verbose", description: "Print the list of extracted files and directories.")
-
-    var optionGroups: [OptionGroup] {
-        let actions = OptionGroup(options: [info, extract], restriction: .exactlyOne)
-        return [actions]
-    }
 
     let archive = Parameter()
 
-    func execute() throws {
-        let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
-                                options: .mappedIfSafe)
-        if info.value {
-            let entries = try SevenZipContainer.info(container: fileData)
-            swcomp.printInfo(entries)
-        } else {
-            let outputPath = self.extract.value!
+    let openFunction: (Data) throws -> [ZipEntry] = ZipContainer.open
+    let infoFunction: (Data) throws -> [ZipEntryInfo] = ZipContainer.info
 
-            if try !isValidOutputDirectory(outputPath, create: true) {
-                print("ERROR: Specified path already exists and is not a directory.")
-                exit(1)
-            }
-
-            let entries = try SevenZipContainer.open(container: fileData)
-            try swcomp.write(entries, outputPath, verbose.value)
-        }
-    }
 }
--- Sources/swcomp/ZipCommand.swift
+++ Sources/swcomp/ZipCommand.swift
@@ -7,40 +7,18 @@ import Foundation
 import SWCompression
 import SwiftCLI
 
-class ZipCommand: Command {
+class ZipCommand: ContainerCommand {
 
     let name = "zip"
     let shortDescription = "Extracts ZIP container"
 
     let info = Flag("-i", "--info", description: "Print list of entries in container and their attributes")
     let extract = Key<String>("-e", "--extract", description: "Extract container into specified directory")
-
     let verbose = Flag("--verbose", description: "Print the list of extracted files and directories.")
-
-    var optionGroups: [OptionGroup] {
-        let actions = OptionGroup(options: [info, extract], restriction: .exactlyOne)
-        return [actions]
-    }
 
     let archive = Parameter()
 
-    func execute() throws {
-        let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
-                                options: .mappedIfSafe)
-        if info.value {
-            let entries = try ZipContainer.info(container: fileData)
-            swcomp.printInfo(entries)
-        } else {
-            let outputPath = self.extract.value!
-
-            if try !isValidOutputDirectory(outputPath, create: true) {
-                print("ERROR: Specified path already exists and is not a directory.")
-                exit(1)
-            }
-
-            let entries = try ZipContainer.open(container: fileData)
-            try swcomp.write(entries, outputPath, verbose.value)
-        }
-    }
+    let openFunction: (Data) throws -> [ZipEntry] = ZipContainer.open
+    let infoFunction: (Data) throws -> [ZipEntryInfo] = ZipContainer.info
 
 }
