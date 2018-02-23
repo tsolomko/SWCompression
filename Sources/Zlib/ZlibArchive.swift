b19382833b19c98e373e1cb63fbb7ff187262b7e
Timofey Solomko <tsolomko@gmail.com>
2018-02-23 12:47:53 +0300

    [Zlib] Add public unarchive(from:) function which uses BBD's ByteReader as its argument

--- Sources/Zlib/ZlibArchive.swift
+++ Sources/Zlib/ZlibArchive.swift
@@ -23,10 +23,25 @@ public class ZlibArchive: Archive {
      - Returns: Unarchived data.
      */
     public static func unarchive(archive data: Data) throws -> Data {
-        /// Object with input data which supports convenient work with bit shifts.
-        let bitReader = LsbBitReader(data: data)
+        return try unarchive(from: ByteReader(data: data))
+    }
 
-        _ = try ZlibHeader(bitReader)
+    /**
+     Unarchives Zlib archive, starting from the current byte offset of `byteReader`.
+
+     - Note: This function is specification compliant.
+
+     - Parameter byteReader: `ByteReader` with Zlib archived data starting from its current byte offset.
+
+     - Throws: `DeflateError` or `ZlibError` depending on the type of the problem.
+     It may indicate that either archive is damaged or it might not be archived with Zlib
+     or compressed with Deflate at all.
+
+     - Returns: Unarchived data.
+     */
+    public static func unarchive(from byteReader: ByteReader) throws -> Data {
+        _ = try ZlibHeader(byteReader)
+        let bitReader = LsbBitReader(byteReader)
 
         let out = try Deflate.decompress(bitReader)
         bitReader.align()
