068b83d62fe880200b40bc4a26ee751b94b5b2c9
Timofey Solomko <tsolomko@gmail.com>
2018-03-03 01:14:51 +0300

    [TAR] Add known extended header fields as properties to TarExtendedHeader

--- Sources/TAR/TarExtendedHeader.swift
+++ Sources/TAR/TarExtendedHeader.swift
@@ -9,6 +9,24 @@ struct TarExtendedHeader {
 
     let entries: [String: String]
 
+    var atime: Double?
+    var ctime: Double?
+    var mtime: Double?
+
+    var size: Int?
+
+    var uid: Int?
+    var gid: Int?
+
+    var uname: String?
+    var gname: String?
+
+    var path: String?
+    var linkpath: String?
+
+    var charset: String?
+    var comment: String?
+
     init?(_ data: Data?) throws {
         guard let data = data, let headerString = String(data: data, encoding: .utf8)
             else { return nil }
@@ -26,8 +44,38 @@ struct TarExtendedHeader {
             let keywordValue = headerEntrySplit[1]
             let keywordValueSplit = keywordValue.split(separator: "=", maxSplits: 1,
                                                        omittingEmptySubsequences: false)
+            let key = String(keywordValueSplit[0])
+            let value = String(keywordValueSplit[1])
+            entries[key] = value
 
-            entries[String(keywordValueSplit[0])] = String(keywordValueSplit[1])
+            switch key {
+            case "uid":
+                self.uid = Int(value)
+            case "gid":
+                self.gid = Int(value)
+            case "uname":
+                self.uname = value
+            case "gname":
+                self.gname = value
+            case "size":
+                self.size = Int(value)
+            case "atime":
+                self.atime = Double(value)
+            case "ctime":
+                self.ctime = Double(value)
+            case "mtime":
+                self.mtime = Double(value)
+            case "path":
+                self.path = value
+            case "linkpath":
+                self.linkpath = value
+            case "charset":
+                self.charset = value
+            case "comment":
+                self.comment = value
+            default:
+                break
+            }
         }
 
         self.entries = entries
