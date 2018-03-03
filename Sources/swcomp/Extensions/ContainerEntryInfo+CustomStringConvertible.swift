f2e7c6c4b301a0f4976799f00170bce4ac7b94a3
Timofey Solomko <tsolomko@gmail.com>
2018-03-03 13:05:50 +0300

    [swcomp] Print addtional properties for all types of entry infos (as well as unknown pax records)

--- Sources/swcomp/Extensions/ContainerEntryInfo+CustomStringConvertible.swift
+++ Sources/swcomp/Extensions/ContainerEntryInfo+CustomStringConvertible.swift
@@ -38,6 +38,54 @@ extension ContainerEntryInfo where Self: CustomStringConvertible {
             if tarEntry.type == .symbolicLink {
                 output += "Linked path: \(tarEntry.linkName)\n"
             }
+            if let ownerID = tarEntry.ownerID {
+                output += "Uid: \(ownerID)\n"
+            }
+            if let groupID = tarEntry.groupID {
+                output += "Gid: \(groupID)\n"
+            }
+            if let ownerUserName = tarEntry.ownerUserName {
+                output += "Uname: \(ownerUserName)\n"
+            }
+            if let ownerGroupName = tarEntry.ownerGroupName {
+                output += "Gname: \(ownerGroupName)\n"
+            }
+            if let comment = tarEntry.comment {
+                output += "Comment: \(comment)\n"
+            }
+            if let unknownPaxRecords = tarEntry.unknownExtendedHeaderRecords, unknownPaxRecords.count > 0 {
+                output += "Unknown PAX (extended header) records:\n"
+                for entry in unknownPaxRecords {
+                    output += "  \(entry.key): \(entry.value)\n"
+                }
+            }
+        }
+
+        if let zipEntry = self as? ZipEntryInfo {
+            output += "Comment: \(zipEntry.comment)\n"
+            output += String(format: "External File Attributes: 0x%08X\n", zipEntry.externalFileAttributes)
+            output += "Is text file: \(zipEntry.isTextFile)\n"
+            output += "File system type: \(zipEntry.fileSystemType)\n"
+            output += "Compression method: \(zipEntry.compressionMethod)\n"
+            if let ownerID = zipEntry.ownerID {
+                output += "Uid: \(ownerID)\n"
+            }
+            if let groupID = zipEntry.groupID {
+                output += "Gid: \(groupID)\n"
+            }
+            
+        }
+
+        if let sevenZipEntry = self as? SevenZipEntryInfo {
+            if let winAttrs = sevenZipEntry.winAttributes {
+                output += String(format: "Win attributes: 0x%08X\n", winAttrs)
+            }
+            if let crc = sevenZipEntry.crc {
+                output += String(format: "CRC32: 0x%08X\n", crc)
+            }
+            output += "Has stream: \(sevenZipEntry.hasStream)\n"
+            output += "Is empty: \(sevenZipEntry.isEmpty)\n"
+            output += "Is anti-file: \(sevenZipEntry.isAnti)\n"
         }
 
         if let size = self.size {
