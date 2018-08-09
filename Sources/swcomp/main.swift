b5b9b570390839bd3b63dd5324b7d2098f6dbd91
Timofey Solomko <tsolomko@gmail.com>
2018-08-09 22:13:36 +0300

    Prepare for 4.4.0 release

--- Sources/swcomp/main.swift
+++ Sources/swcomp/main.swift
@@ -7,7 +7,7 @@ import Foundation
 import SWCompression
 import SwiftCLI
 
-let cli = CLI(name: "swcomp", version: "4.4.0-test",
+let cli = CLI(name: "swcomp", version: "4.4.0",
               description: """
                            swcomp - small command-line client for SWCompression framework.
                            Serves as an example of SWCompression usage.
