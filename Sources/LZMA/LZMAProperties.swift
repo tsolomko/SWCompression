726e067fe46eda115c2f54adc60995fe81a25785
Timofey Solomko <tsolomko@gmail.com>
2018-08-13 12:24:50 +0300

    [LZMA] Add LZMAProperties struct with all properties (lc, lp, pb and dict size)

---
+++ Sources/LZMA/LZMAProperties.swift
@@ -0,0 +1,13 @@
+// Copyright (c) 2018 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+public struct LZMAProperties {
+
+    public var lc: Int
+    public var lp: Int
+    public var pb: Int
+    public var dictionarySize: Int
+
+}
