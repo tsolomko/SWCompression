7ca677c3d5e0ff795da8e51c53288bb16b5a0ad0
Timofey Solomko <tsolomko@gmail.com>
2018-08-13 20:48:06 +0300

    [LZMA] Add several initializers to LZMAProperties
    These include: init-with-zeros, init-with values, and init from a single byte using standard LZMA properties encoding scheme.

--- Sources/LZMA/LZMAProperties.swift
+++ Sources/LZMA/LZMAProperties.swift
@@ -16,4 +16,28 @@ public struct LZMAProperties {
         }
     }
 
+    public init(lc: Int, lp: Int, pb: Int, dictionarySize: Int) {
+        self.lc = lc
+        self.lp = lp
+        self.pb = pb
+        self.dictionarySize = dictionarySize
+    }
+
+    init() {
+        self.lc = 0
+        self.lp = 0
+        self.pb = 0
+        self.dictionarySize = 0
+    }
+
+    init(lzmaByte: UInt8) {
+        let intByte = lzmaByte.toInt()
+
+        self.lc = intByte % 9
+        self.pb = (intByte / 9) / 5
+        self.lp = (intByte / 9) % 5
+
+        self.dictionarySize = 0
+    }
+
 }
