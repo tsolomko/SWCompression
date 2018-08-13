e6d680e82bb857c4c13c279fb05b7687c207d308
Timofey Solomko <tsolomko@gmail.com>
2018-08-13 20:19:16 +0300

    [LZMA] Add observer to LZMAProperties.dictionarySize which keeps it max bounded

--- Sources/LZMA/LZMAProperties.swift
+++ Sources/LZMA/LZMAProperties.swift
@@ -8,6 +8,12 @@ public struct LZMAProperties {
     public var lc: Int
     public var lp: Int
     public var pb: Int
-    public var dictionarySize: Int
+    public var dictionarySize: Int {
+        didSet {
+            if dictionarySize < 1 << 12 {
+                dictionarySize = 1 << 12
+            }
+        }
+    }
 
 }
