1df768c09f3caaa51036c2ba64a545dea2397fb4
Timofey Solomko <tsolomko@gmail.com>
2019-01-04 20:20:26 +0300

    Move the lengths bootstrapping function into the namespace of the CodeLength

--- Sources/Deflate/Deflate+Constants.swift
+++ Sources/Deflate/Deflate+Constants.swift
@@ -9,8 +9,8 @@ extension Deflate {
 
     struct Constants {
         // Bootstraps for Static Huffman trees (first element in tuple is code, second is number of bits).
-        static let staticHuffmanBootstrap = Deflate.lengths(from: [(0, 8), (144, 9), (256, 7), (280, 8), (288, -1)])
-        static let staticHuffmanDistancesBootstrap = Deflate.lengths(from: [(0, 5), (32, -1)])
+        static let staticHuffmanBootstrap = CodeLength.lengths(from: [(0, 8), (144, 9), (256, 7), (280, 8), (288, -1)])
+        static let staticHuffmanDistancesBootstrap = CodeLength.lengths(from: [(0, 5), (32, -1)])
 
         static let codeLengthOrders: [Int] =
             [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
