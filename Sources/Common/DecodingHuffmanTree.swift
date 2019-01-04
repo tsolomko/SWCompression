08bea5ad53515f032de319516b4ef17eedc3b1cd
Timofey Solomko <tsolomko@gmail.com>
2019-01-04 20:06:58 +0300

    Make Huffman tree classes final

--- Sources/Common/DecodingHuffmanTree.swift
+++ Sources/Common/DecodingHuffmanTree.swift
@@ -6,7 +6,7 @@
 import Foundation
 import BitByteData
 
-class DecodingHuffmanTree {
+final class DecodingHuffmanTree {
 
     private let bitReader: BitReader
 
--- Sources/Common/EncodingHuffmanTree.swift
+++ Sources/Common/EncodingHuffmanTree.swift
@@ -6,7 +6,7 @@
 import Foundation
 import BitByteData
 
-class EncodingHuffmanTree {
+final class EncodingHuffmanTree {
 
     private let bitWriter: BitWriter
 
