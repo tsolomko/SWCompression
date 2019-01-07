7ae5cfa1a2c32996b0a255cc8942d9b06d970cb6
Timofey Solomko <tsolomko@gmail.com>
2019-01-07 13:36:42 +0300

    Add a new struct which represents an abstract code from coding tree

---
+++ Sources/Common/CodingTree/Code.swift
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+import Foundation
+
+struct Code {
+
+    /// Number of bits used for `code`.
+    let bits: Int
+    let code: Int
+    let symbol: Int
+
+}
