fa3aeece25f4789d297ef5886688cf49acb2c53f
Timofey Solomko <tsolomko@gmail.com>
2020-04-06 19:01:34 +0300

    Convert remaining Utils bash scripts into utils.py commands

--- utils.py
+++ utils.py
@@ -41,7 +41,7 @@ def _ci_script_macos():
     
     for destination, action in destinations_actions:
         xcodebuild_command = xcodebuild_command_parts + destination + action
-        print("+ {0}".format(" ".join(xcodebuild_command)))
+        print("+ {0} | xcpretty -f `xcpretty-travis-formatter`".format(" ".join(xcodebuild_command)))
         xcodebuild_process = subprocess.Popen(xcodebuild_command, stdout=subprocess.PIPE)
         xcpretty_command = ["xcpretty", "-f", "`xcpretty-travis-formatter`"]
         subprocess.run(xcpretty_command, stdin=xcodebuild_process.stdout, shell=True, check=True)
@@ -58,14 +58,52 @@ def action_ci(args):
     else:
         raise Exception("Unknown CI command")
 
+def action_cw(args):
+    _sprun(["rm", "-rf", "build/"])
+    _sprun(["rm", "-rf", "Carthage/"])
+    _sprun(["rm", "-rf", "docs/"])
+    _sprun(["rm", "-rf", "Pods/"])
+    _sprun(["rm", "-rf", ".build/"])
+    _sprun(["rm", "-f", "Cartfile.resolved"])
+    _sprun(["rm", "-f", "docs.json"])
+    _sprun(["rm", "-f", "Package.resolved"])
+    _sprun(["rm", "-f", "SWCompression.framework.zip"])
+
+def _pw_macos():
+    print("=> Downloading dependency (BitByteData) using Carthage")
+    _sprun(["carthage", "bootstrap"])
+
+def action_pw(args):
+    if args.os == "macos":
+        _pw_macos()
+    else:
+        raise Exception("Unknown OS")
+    if not args.no_test_files:
+        print("=> Downloading files used for testing")
+        _sprun(["git", "submodule", "update", "--init", "--recursive"])
+
 parser = argparse.ArgumentParser(description="A tool with useful commands for developing SWCompression")
 subparsers = parser.add_subparsers(title="commands", help="a command to perform", metavar="CMD")
 
 # Parser for 'ci' command.
-parser_ci = subparsers.add_parser("ci", help="a set of commands used by CI", description="a set of commands used by CI")
+parser_ci = subparsers.add_parser("ci", help="a subset of commands used by CI",
+                                    description="a subset of commands used by CI")
 parser_ci.add_argument("cmd", choices=["before-deploy", "install-macos", "script-linux", "script-macos"],
                         help="a command to perform on CI", metavar="CI_CMD")
 parser_ci.set_defaults(func=action_ci)
+
+# Parser for 'cleanup-workspace' command.
+parser_cw = subparsers.add_parser("cleanup-workspace", help="cleanup workspace",
+                            description="cleans workspace from files produced by various build systems")
+parser_cw.set_defaults(func=action_cw)
+
+# Parser for 'prepare-workspace' command.
+parser_pw = subparsers.add_parser("prepare-workspace", help="prepare workspace",
+                            description="prepares workspace for developing SWCompression")
+parser_pw.add_argument("os", choices=["macos"], help="development operating system", metavar="OS")
+parser_pw.add_argument("--no-test-files", "-T", action="store_true", dest="no_test_files",
+                        help="don't download example files used for testing")
+parser_pw.set_defaults(func=action_pw)
 
 args = parser.parse_args()
 args.func(args)
