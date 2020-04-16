153f805bd478c6d1e6bd899bb4e47d28ac2743b7
Timofey Solomko <tsolomko@gmail.com>
2020-04-16 13:42:37 +0300

    CI changes: use ubuntu-18.04 on travis; run azp on all branches except master; don't use xcpretty and don't install it; remove azp-specific utils commands; set some env vars via os python module in utils.py to fix swift on linux issues; use -quiet argument for xcode to fix travis log length issues; remove verbosity from ci install linux step

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import subprocess
 import sys
 
@@ -26,22 +27,13 @@ def _ci_before_deploy():
 def _ci_install_macos():
     _sprun(["brew", "install", "git-lfs"])
     _sprun(["git", "lfs", "install"])
-    _sprun(["gem", "install", "-N", "xcpretty-travis-formatter"])
-
-def _ci_install_macos_azp():
-    _sprun(["brew", "install", "git-lfs"])
-    _sprun(["git", "lfs", "install"])
-    _sprun(["gem", "install", "-N", "xcpretty-azure-pipelines-formatter"])
 
 def _ci_install_linux():
-    _sprun(["curl -sL https://swiftenv.fuller.li/install.sh > install.sh"], shell=True)
-    _sprun(["cat", "install.sh"])
-    _sprun(["chmod", "+x", "install.sh"])
-    _sprun(["./install.sh"], shell=True)
+    _sprun(["eval \"$(curl -sL https://swiftenv.fuller.li/install.sh)\""], shell=True)
 
 def _ci_script_macos():
     _sprun(["swift", "--version"])
-    xcodebuild_command_parts = ["xcodebuild", "-project", "SWCompression.xcodeproj", "-scheme", "SWCompression"]
+    xcodebuild_command_parts = ["xcodebuild", "-quiet", "-project", "SWCompression.xcodeproj", "-scheme", "SWCompression"]
     destinations_actions = [(["-destination 'platform=OS X'"], ["clean", "test"]), 
                     (["-destination 'platform=iOS Simulator,name=iPhone 8'"], ["clean", "test"]), 
                     (["-destination 'platform=watchOS Simulator,name=Apple Watch - 38mm'"], ["clean", "build"]), 
@@ -49,44 +41,25 @@ def _ci_script_macos():
     
     for destination, action in destinations_actions:
         xcodebuild_command = xcodebuild_command_parts + destination + action
-        print("+ {0} | xcpretty -f `xcpretty-travis-formatter`".format(" ".join(xcodebuild_command)))
-        xcodebuild_process = subprocess.Popen(xcodebuild_command, stdout=subprocess.PIPE)
-        xcpretty_command = ["xcpretty", "-f", "`xcpretty-travis-formatter`"]
-        subprocess.run(xcpretty_command, stdin=xcodebuild_process.stdout, shell=True, check=True)
-
-def _ci_script_macos_azp():
-    _sprun(["swift", "--version"])
-    xcodebuild_command_parts = ["xcodebuild", "-project", "SWCompression.xcodeproj", "-scheme", "SWCompression"]
-    destinations_actions = [(["-destination 'platform=OS X'"], ["clean", "test"]), 
-                    (["-destination 'platform=iOS Simulator,name=iPhone 8'"], ["clean", "test"]), 
-                    (["-destination 'platform=watchOS Simulator,name=Apple Watch - 38mm'"], ["clean", "build"]), 
-                    (["-destination 'platform=tvOS Simulator,name=Apple TV'"], ["clean", "test"])]
-    
-    for destination, action in destinations_actions:
-        xcodebuild_command = xcodebuild_command_parts + destination + action
-        print("+ {0} | xcpretty -f `xcpretty-azure-pipelines-formatter`".format(" ".join(xcodebuild_command)))
-        xcodebuild_process = subprocess.Popen(xcodebuild_command, stdout=subprocess.PIPE)
-        xcpretty_command = ["xcpretty", "-f", "`xcpretty-azure-pipelines-formatter`"]
-        subprocess.run(xcpretty_command, stdin=xcodebuild_process.stdout, shell=True, check=True)
+        _sprun(xcodebuild_command)
 
 def _ci_script_linux():
-    _sprun(["swift", "--version"])
-    _sprun(["swift", "build"])
-    _sprun(["swift", "build", "-c", "release"])
+    env = os.environ.copy()
+    env["SWIFTENV_ROOT"] = env["HOME"] +"/.swiftenv"
+    env["PATH"] = env["SWIFTENV_ROOT"] + "/bin:" + env["SWIFTENV_ROOT"] + "/shims:"+ env["PATH"]
+    _sprun(["swift", "--version"], env=env)
+    _sprun(["swift", "build"], env=env)
+    _sprun(["swift", "build", "-c", "release"], env=env)
 
 def action_ci(args):
     if args.cmd == "before-deploy":
         _ci_before_deploy()
     elif args.cmd == "install-macos":
         _ci_install_macos()
-    elif args.cmd == "install-macos-azp":
-        _ci_install_macos_azp()
     elif args.cmd == "install-linux":
         _ci_install_linux()
     elif args.cmd == "script-macos":
         _ci_script_macos()
-    elif args.cmd == "script-macos-azp":
-        _ci_script_macos_azp()
     elif args.cmd == "script-linux":
         _ci_script_linux()
     else:
@@ -127,8 +100,7 @@ subparsers = parser.add_subparsers(title="commands", help="a command to perform"
 # Parser for 'ci' command.
 parser_ci = subparsers.add_parser("ci", help="a subset of commands used by CI",
                                     description="a subset of commands used by CI")
-parser_ci.add_argument("cmd", choices=["before-deploy", "install-macos", "install-macos-azp", "install-linux",
-                                        "script-macos", "script-macos-azp", "script-linux"],
+parser_ci.add_argument("cmd", choices=["before-deploy", "install-macos", "install-linux", "script-macos", "script-linux"],
                         help="a command to perform on CI", metavar="CI_CMD")
 parser_ci.set_defaults(func=action_ci)
 
