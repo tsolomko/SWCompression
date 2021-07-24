3e96a84f6227828590d9e885d92abd5f277b2a41
Timofey Solomko <tsolomko@gmail.com>
2021-07-24 14:23:10 +0300

    [TAR] Add a docs note to TarCreateError about it being never used

--- Sources/TAR/TarCreateError.swift
+++ Sources/TAR/TarCreateError.swift
@@ -6,9 +6,13 @@
 /**
  Represents an error which happened during the creation of a new TAR container.
 
- - Note: This error type is going to be merged with `TarError` in the next major update.
+ - Note: This error type is never used and will be removed in the next major update.
  */
 public enum TarCreateError: Error {
-    /// One of the `TarEntryInfo`'s string properties (such as `name`) cannot be encoded with UTF-8 encoding.
+    /**
+     One of the `TarEntryInfo`'s string properties (such as `name`) cannot be encoded with UTF-8 encoding.
+
+     - Note: This error is never thrown and will be removed in the next major update.
+     */
     case utf8NonEncodable
 }
