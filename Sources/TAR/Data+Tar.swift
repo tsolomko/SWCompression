11a2054498d3a0ddc1ff9673d75d4696655ee192
Timofey Solomko <tsolomko@gmail.com>
2021-07-24 13:56:52 +0300

    [TAR] Move Data extensions from TarEntryInfo into separate file
    In addition Data.append(tarString:maxLength:) has been modified to not be throwing.

--- Sources/TAR/Data+Tar.swift
+++ Sources/TAR/Data+Tar.swift
@@ -14,4 +14,46 @@ extension Data {
         self.append(Data(count: paddingSize))
     }
 
+    mutating func append(tarInt value: Int?, maxLength: Int) {
+        guard var value = value else {
+            // No value; fill field with NULLs.
+            self.append(Data(count: maxLength))
+            return
+        }
+
+        let maxOctalValue = (1 << (maxLength * 3)) - 1
+        guard value > maxOctalValue || value < 0 else {
+            // Normal octal encoding.
+            self.append(Data(String(value, radix: 8).utf8).zeroPad(maxLength))
+            return
+        }
+
+        // Base-256 encoding.
+        var buffer = Array(repeating: 0 as UInt8, count: maxLength)
+        for i in stride(from: maxLength - 1, to: 0, by: -1) {
+            buffer[i] = UInt8(truncatingIfNeeded: value & 0xFF)
+            value >>= 8
+        }
+        buffer[0] |= 0x80 // Highest bit indicates base-256 encoding.
+        self.append(Data(buffer))
+    }
+
+    mutating func append(tarString string: String?, maxLength: Int) {
+        guard let string = string else {
+            // No value; fill field with NULLs.
+            self.append(Data(count: maxLength))
+            return
+        }
+        self.append(Data(string.utf8).zeroPad(maxLength))
+    }
+
+    /// This should work in the same way as `String.padding(toLength: length, withPad: "\0", startingAt: 0)`.
+    @inline(__always)
+    private func zeroPad(_ length: Int) -> Data {
+        // TODO: Maybe this should modify self
+        var out = length < self.count ? self.prefix(upTo: length) : self
+        out.append(Data(count: length - out.count))
+        return out
+    }
+
 }
