46590ad631241a9c433623ab4d5bf055e5dad9ce
Timofey Solomko <tsolomko@gmail.com>
2021-09-24 18:49:18 +0300

    [LZ4] Add an empty implementation of xxHash-32

---
+++ Sources/LZ4/XxHash32.swift
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+import Foundation
+
+enum XxHash32 {
+
+    static func hash(data: Data, seed: UInt32 = 0) -> [UInt8] {
+        // TODO:
+        return [0, 0, 0, 0]
+    }
+
+}
