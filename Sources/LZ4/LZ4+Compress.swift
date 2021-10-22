e860b9d4a9c5ce4baa15f71ee134dbf0b7eb61a0
Timofey Solomko <tsolomko@gmail.com>
2021-10-22 19:27:23 +0300

    [LZ4] Slightly optimize match calculation in compression

--- Sources/LZ4/LZ4+Compress.swift
+++ Sources/LZ4/LZ4+Compress.swift
@@ -161,7 +161,7 @@ extension LZ4: CompressionAlgorithm {
         // are checked for this condition in the match-searching while-loop, but minmatches (4-bytes long) are verified
         // here (hence -9).
         while i < blockBytes.endIndex - 9 {
-            let matchId = LZ4.combine(blockBytes, in: i..<i + 4)
+            let matchId = LZ4.combine(blockBytes, from: i)
             guard let matchStartIndex = matchStorage[matchId] else {
                 // No match found.
                 // We need to save where we met this four-byte sequence.
@@ -270,16 +270,16 @@ extension LZ4: CompressionAlgorithm {
             else { return [:] }
         var matchStorage = [UInt32: Int]()
         for i in dict.startIndex..<dict.endIndex - 4 {
-            let matchId = LZ4.combine(dict, in: i..<i + 4)
+            let matchId = LZ4.combine(dict, from: i)
             matchStorage[matchId] = i
         }
         return matchStorage
     }
 
     @inline(__always)
-    private static func combine(_ bytes: [UInt8], in range: CountableRange<Int>) -> UInt32 {
+    private static func combine(_ bytes: [UInt8], from index: Array.Index) -> UInt32 {
         var result = 0 as UInt32
-        for i in range {
+        for i in index..<index + 4 {
             result <<= 8
             result |= UInt32(truncatingIfNeeded: bytes[i])
         }
