d5b495689bbe386704abdf15eb6a89b9f2f8be3d
Timofey Solomko <tsolomko@gmail.com>
2021-11-17 21:43:00 +0300

    Add version-specific manifests for SPM 5.2

---
+++ Package@swift-5.2.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "SWCompression",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(
+            name: "SWCompression",
+            targets: ["SWCompression"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jakeheis/SwiftCLI",
+                 from: "6.0.0"),
+        .package(url: "https://github.com/tsolomko/BitByteData",
+                 from: "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SWCompression",
+            dependencies: ["BitByteData"],
+            path: "Sources",
+            sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"]),
+        .target(
+            name: "swcomp",
+            dependencies: ["SWCompression", "SwiftCLI"],
+            path: "Sources",
+            sources: ["swcomp"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
