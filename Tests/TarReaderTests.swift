245f0430253feb434c3c4251404dd1fdddbe0c41
Timofey Solomko <tsolomko@gmail.com>
2022-01-08 14:43:44 +0300

    [Tests] Move TarReader/Writer tests into their own files

---
+++ Tests/TarReaderTests.swift
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+import XCTest
+import SWCompression
+
+class TarReaderTests: XCTestCase {
+
+    private static let testType: String = "tar"
+
+    func test() throws {
+        let testHandle = try Constants.handle(forTest: "test", withType: TarReaderTests.testType)
+        var reader = TarReader(fileHandle: testHandle)
+        let entries = [try reader.read()!]
+        if #available(macOS 10.15.4, iOS 13.4, watchOS 6.2, tvOS 13.4, *) {
+            try testHandle.close()
+        } else {
+            testHandle.closeFile()
+        }
+
+        XCTAssertEqual(entries.count, 1)
+        XCTAssertEqual(entries[0].info.name, "test5.answer")
+        XCTAssertEqual(entries[0].info.size, 0)
+        XCTAssertEqual(entries[0].info.type, .regular)
+        XCTAssertEqual(entries[0].info.ownerID, 501)
+        XCTAssertEqual(entries[0].info.groupID, 20)
+        XCTAssertEqual(entries[0].info.ownerUserName, "timofeysolomko")
+        XCTAssertEqual(entries[0].info.ownerGroupName, "staff")
+        XCTAssertEqual(entries[0].info.permissions, Permissions(rawValue: 420))
+        XCTAssertNil(entries[0].info.comment)
+        XCTAssertEqual(entries[0].data, Data())
+    }
+
+}
--- Tests/TarTests.swift
+++ Tests/TarTests.swift
@@ -41,29 +41,6 @@ class TarTests: XCTestCase {
         XCTAssertEqual(entries[0].data, Data())
     }
 
-    func testHandle() throws {
-        let testHandle = try Constants.handle(forTest: "test", withType: TarTests.testType)
-        var reader = TarReader(fileHandle: testHandle)
-        let entries = [try reader.read()!]
-        if #available(macOS 10.15.4, iOS 13.4, watchOS 6.2, tvOS 13.4, *) {
-            try testHandle.close()
-        } else {
-            testHandle.closeFile()
-        }
-
-        XCTAssertEqual(entries.count, 1)
-        XCTAssertEqual(entries[0].info.name, "test5.answer")
-        XCTAssertEqual(entries[0].info.size, 0)
-        XCTAssertEqual(entries[0].info.type, .regular)
-        XCTAssertEqual(entries[0].info.ownerID, 501)
-        XCTAssertEqual(entries[0].info.groupID, 20)
-        XCTAssertEqual(entries[0].info.ownerUserName, "timofeysolomko")
-        XCTAssertEqual(entries[0].info.ownerGroupName, "staff")
-        XCTAssertEqual(entries[0].info.permissions, Permissions(rawValue: 420))
-        XCTAssertNil(entries[0].info.comment)
-        XCTAssertEqual(entries[0].data, Data())
-    }
-
     func testPax() throws {
         let testData = try Constants.data(forTest: "full_test", withType: TarTests.testType)
 
