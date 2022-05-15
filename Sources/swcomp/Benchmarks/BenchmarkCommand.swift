c407bcfb9c7c478191849115b5ca1c2b40057cd2
Timofey Solomko <tsolomko@gmail.com>
2022-05-15 16:20:41 +0300

    [swcomp] Add comparison with saved results via --compare option

--- Sources/swcomp/Benchmarks/BenchmarkCommand.swift
+++ Sources/swcomp/Benchmarks/BenchmarkCommand.swift
@@ -22,6 +22,9 @@ final class BenchmarkCommand: Command {
     @Key("-s", "--save", description: "Saves the results into the specified file")
     var savePath: String?
 
+    @Key("-c", "--compare", description: "Compares the results with the results saved in the specified file")
+    var comparePath: String?
+
     @Flag("-W", "--no-warmup", description: "Disables warmup iteration")
     var noWarmup: Bool
 
@@ -39,6 +42,12 @@ final class BenchmarkCommand: Command {
         print(title)
 
         var results = [BenchmarkResult]()
+        var otherResults: [BenchmarkResult]? = nil
+        if let comparePath = comparePath {
+            let data = try Data(contentsOf: URL(fileURLWithPath: comparePath))
+            let decoder = JSONDecoder()
+            otherResults = try decoder.decode(Array<BenchmarkResult>.self, from: data)
+        }
 
         for input in self.inputs {
             print("Input: \(input)")
@@ -76,8 +85,45 @@ final class BenchmarkCommand: Command {
             let std = sqrt(squareSum / Double(iterationCount) - sum * sum / Double(iterationCount * iterationCount))
             print("Standard deviation: " + benchmark.format(std))
 
-            results.append(BenchmarkResult(name: self.selectedBenchmark.rawValue, input: input, iterCount: iterationCount,
-                                           avg: avgSpeed, std: std))
+            let result = BenchmarkResult(name: self.selectedBenchmark.rawValue, input: input, iterCount: iterationCount,
+                                         avg: avgSpeed, std: std)
+            if let other = otherResults?.first(where: { $0.name == result.name && $0.input == result.input }) {
+                let comparison = result.compare(with: other)
+                let diff = (result.avg / other.avg - 1) * 100
+                if diff < 0 {
+                    switch comparison {
+                    case 1:
+                        print(String(format: "OK  %f%% (p-value > 0.05)", diff))
+                    case nil:
+                        print("Cannot compare due to unsupported iteration count.")
+                    case -1:
+                        print(String(format: "REG %f%% (p-value < 0.05)", diff))
+                    case 0:
+                        print(String(format: "REG %f%% (p-value = 0.05)", diff))
+                    default:
+                        print("ERROR: Unknown comparison result.")
+                        exit(1)
+                    }
+                }
+                else if diff > 0 {
+                    switch comparison {
+                    case 1:
+                        print(String(format: "OK  %f%% (p-value > 0.05)", diff))
+                    case nil:
+                        print("Cannot compare due to unsupported iteration count.")
+                    case -1:
+                        print(String(format: "IMP %f%% (p-value < 0.05)", diff))
+                    case 0:
+                        print(String(format: "IMP %f%% (p-value = 0.05)", diff))
+                    default:
+                        print("ERROR: Unknown comparison result.")
+                        exit(1)
+                    }
+                } else {
+                    print("OK (exact match of averages)")
+                }
+            }
+            results.append(result)
 
             print()
         }
--- Sources/swcomp/Benchmarks/BenchmarkResult.swift
+++ Sources/swcomp/Benchmarks/BenchmarkResult.swift
@@ -6,9 +6,34 @@
 import Foundation
 
 struct BenchmarkResult: Codable {
+
     var name: String
     var input: String
     var iterCount: Int
     var avg: Double
     var std: Double
+
+    func compare(with other: BenchmarkResult) -> Int? {
+        let degreesOfFreedom = Double(self.iterCount + other.iterCount - 2)
+        let t1: Double = Double(self.iterCount - 1) * pow(self.std, 2)
+        let t2: Double = Double(other.iterCount - 1) * pow(other.std, 2)
+        let pooledStd = ((t1 + t2) / degreesOfFreedom).squareRoot()
+        let se = pooledStd * (1 / Double(self.iterCount) + 1 / Double(other.iterCount)).squareRoot()
+        let tStat = (self.avg - other.avg ) / se
+        if degreesOfFreedom == 18 {
+            if abs(tStat) > 2.101 {
+                // p-value < 0.05
+                return -1
+            } else if abs(tStat) == 2.101 {
+                // p-value = 0.05
+                return 0
+            } else {
+                // p-value > 0.05
+                return 1
+            }
+        } else {
+            return nil
+        }
+    }
+
 }
