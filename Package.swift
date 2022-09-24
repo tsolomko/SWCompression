f7f9ccaeb121a45b2c1abec526fe7a7e046c6758
Timofey Solomko <tsolomko@gmail.com>
2022-09-24 13:26:37 +0300

    Enable swcomp in SPM manifest by default

--- Package.swift
+++ Package.swift
@@ -9,24 +9,22 @@ let package = Package(
             targets: ["SWCompression"]),
     ],
     dependencies: [
-        // SWCOMP: Uncomment the line below to build swcomp example program.
-        // .package(url: "https://github.com/jakeheis/SwiftCLI",
-        //          from: "6.0.0"),
         .package(url: "https://github.com/tsolomko/BitByteData",
                  from: "2.0.0"),
+        .package(url: "https://github.com/jakeheis/SwiftCLI",
+                 from: "6.0.0"),
     ],
     targets: [
-        // SWCOMP: Uncomment the lines below to build swcomp example program.
-        // .target(
-        //     name: "swcomp",
-        //     dependencies: ["SWCompression", "SwiftCLI"],
-        //     path: "Sources",
-        //     sources: ["swcomp"]),
         .target(
             name: "SWCompression",
             dependencies: ["BitByteData"],
             path: "Sources",
             sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"]),
+        .target(
+            name: "swcomp",
+            dependencies: ["SWCompression", "SwiftCLI"],
+            path: "Sources",
+            sources: ["swcomp"]),
     ],
     swiftLanguageVersions: [.v5]
 )
