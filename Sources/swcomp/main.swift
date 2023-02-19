1e7393dc54c2ec0a698e6eb1d6b9989624104447
Timofey Solomko <tsolomko@gmail.com>
2023-02-19 17:00:16 +0200

    Prepare for 4.8.5 release

--- Sources/swcomp/main.swift
+++ Sources/swcomp/main.swift
@@ -7,7 +7,7 @@ import Foundation
 import SWCompression
 import SwiftCLI
 
-let _SWC_VERSION = "4.8.4"
+let _SWC_VERSION = "4.8.5"
 
 let cli = CLI(name: "swcomp", version: _SWC_VERSION,
               description: """
