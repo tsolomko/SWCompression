60b76e64908f26eaa68a88fc7f8f1d2a004c2edd
Timofey Solomko <tsolomko@gmail.com>
2024-04-21 17:24:19 +0300

    [swcomp] Enable swcomp in the SPM manifest only on macOS
    Mainly, because SwiftCLI is incompatible with Linux and Windows, and we both don't need swcomp on these platforms (at the moment), and I am too busy to try to make a fork of SwiftCLI which fixes these issues.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let package = Package(
+var package = Package(
     name: "SWCompression",
     platforms: [
         .macOS(.v11),
@@ -19,8 +19,6 @@ let package = Package(
     dependencies: [
         .package(name: "BitByteData", url: "https://github.com/tsolomko/BitByteData",
                  from: "2.0.0"),
-        .package(name: "SwiftCLI", url: "https://github.com/jakeheis/SwiftCLI",
-                 from: "6.0.0"),
     ],
     targets: [
         .target(
@@ -30,12 +28,13 @@ let package = Package(
             exclude: ["swcomp"],
             sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"],
             resources: [.copy("PrivacyInfo.xcprivacy")]),
-        .target(
-            name: "swcomp",
-            dependencies: ["SWCompression", "SwiftCLI"],
-            path: "Sources",
-            exclude: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib", "PrivacyInfo.xcprivacy"],
-            sources: ["swcomp"]),
     ],
     swiftLanguageVersions: [.v5]
 )
+
+#if os(macOS)
+package.dependencies.append(.package(name: "SwiftCLI", url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"))
+package.targets.append(.executableTarget(name: "swcomp", dependencies: ["SWCompression", "SwiftCLI"], path: "Sources",
+            exclude: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib", "PrivacyInfo.xcprivacy"],
+            sources: ["swcomp"]))
+#endif
