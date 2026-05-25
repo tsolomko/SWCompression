7ee42a166c9eb79dfcd8834259b3714e91f90b67
Timofey Solomko <tsolomko@gmail.com>
2026-05-25 16:40:48 +0800

    Increase minimum deployment targets on Apple platforms

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 var package = Package(
     name: "SWCompression",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17),
-        .tvOS(.v17),
-        .watchOS(.v10),
-        .visionOS(.v1)
+        .macOS(.v15),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2)
     ],
     products: [
         .library(
