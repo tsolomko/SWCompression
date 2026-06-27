8a2dcb53e2a75bd7af50f3ff2fb2716631acb99f
Timofey Solomko <tsolomko@gmail.com>
2026-06-27 17:08:41 +0800

    [swcomp] Remove benchmark convert command

--- Sources/swcomp/Benchmarks/BenchmarkGroup.swift
+++ Sources/swcomp/Benchmarks/BenchmarkGroup.swift
@@ -15,8 +15,7 @@ final class BenchmarkGroup: CommandGroup {
     let children: [Routable] = [
         RunBenchmarkCommand(),
         ShowBenchmarkCommand(),
-        RemoveRunCommand(),
-        ConvertCommand()
+        RemoveRunCommand()
     ]
 
 }
