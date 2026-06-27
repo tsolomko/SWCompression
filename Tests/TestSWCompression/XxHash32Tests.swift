25edf518a90a5e7d92c656712a52ad80048e2f26
Timofey Solomko <tsolomko@gmail.com>
2026-06-27 13:01:59 +0800

    [Tests] Convert XxHash32 tests to use Swift Testing

--- Tests/TestSWCompression/XxHash32Tests.swift
+++ Tests/TestSWCompression/XxHash32Tests.swift
@@ -4,58 +4,58 @@
 // See LICENSE for license information
 
 import Foundation
-import XCTest
+import Testing
 @testable import SWCompression
 
-class XxHash32Tests: XCTestCase {
+struct XxHash32Tests {
 
-    func test1() {
+    @Test func test1() {
         let message = ""
         let answer = 0x02cc5d05 as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test2() {
+    @Test func test2() {
         let message = "a"
         let answer = 0x550d7456 as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test3() {
+    @Test func test3() {
         let message = "abc"
         let answer = 0x32d153ff as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test4() {
+    @Test func test4() {
         let message = "message digest"
         let answer = 0x7c948494 as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test5() {
+    @Test func test5() {
         let message = "abcdefghijklmnopqrstuvwxyz"
         let answer = 0x63a14d5f as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test6() {
+    @Test func test6() {
         let message = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         let answer = 0x9c285e64 as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
-    func test7() {
+    @Test func test7() {
         let message = "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
         let answer = 0x9c05f475 as UInt32
         let hash = XxHash32.hash(data: Data(message.utf8))
-        XCTAssertEqual(hash, answer)
+        #expect(hash == answer)
     }
 
 }
